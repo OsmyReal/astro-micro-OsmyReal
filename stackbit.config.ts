28721e3dd7b064b9475e9fcff9ca021e28e6efbf
OsmyReal <osmyreal@gmail.com>
2025-02-26 01:53:24 -0500

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,44 +1,23 @@
-import { defineStackbitConfig, SiteMapEntry } from "@stackbit/types";
+// stackbit.config.ts
+import { defineStackbitConfig } from "@stackbit/types";
 import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
+  // ...
   contentSources: [
     new GitContentSource({
-      rootPath: __dirname, // Ruta raíz de tu proyecto
-      contentDirs: ["src/content/blog", "src/content/projects"],  // Las carpetas donde está el contenido
+      rootPath: __dirname,
+      contentDirs: ["content"],
       models: [
         {
-          name: "Blog",
-          type: "page",  // El tipo de contenido es 'page'
-          urlPath: "/blog/{slug}",  // Las páginas del blog tendrán URLs como /blog/{slug}
-          filePath: "src/content/blog/{slug}.md",  // Define la ruta a los archivos de blog Markdown
-          fields: [{ name: "title", type: "string", required: true }]  // Definir el título como campo requerido
-        },
-        {
-          name: "Project",
-          type: "page",  // El tipo de contenido es 'page'
-          urlPath: "/projects/{slug}",  // Las páginas de los proyectos tendrán URLs como /projects/{slug}
-          filePath: "src/content/projects/{slug}.md",  // Define la ruta a los archivos de proyectos Markdown
-          fields: [{ name: "title", type: "string", required: true }]  // Definir el título como campo requerido
+          name: "Page",
+          // Define the model as a page model
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.json",
+          fields: [{ name: "title", type: "string", required: true }]
         }
-      ]
+      ],
     })
-  ],
-  siteMap: ({ documents, models }) => {
-    const pageModels = models.filter((m) => m.type === "page");
-
-    return documents
-      .filter((d) => pageModels.some(m => m.name === d.modelName))
-      .map((document) => {
-        const urlModel = document.modelName === "Blog" ? 'blog' : 'project';
-
-        return {
-          stableId: document.id,
-          urlPath: `/${urlModel}/${document.id}`,  // URL para blog y proyectos
-          document,
-          isHomePage: false,
-        };
-      })
-      .filter(Boolean) as SiteMapEntry[];
-  }
+  ]
 });
